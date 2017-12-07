2fa74968921dbc591a45bcd3e9a0131f653dc60e
habibmasri <habib.masri@gmail.com>
2017-12-07 10:13:59 +0200

    Create copyMRBlogs.sh

---
+++ copyMRBlogs.sh
@@ -0,0 +1,3 @@
+cp ./logs/mxmarketrisk_service/mxmarketrisk_engine/mr_engine_0/service.log ./"$1"/
+cp ./logs/var/compute_fullreval*.time ./"$1"/
+cp ./logs/mxmarketrisk_service/mxmarketrisk_engine_mx/tasks*.log ./"$1"/
